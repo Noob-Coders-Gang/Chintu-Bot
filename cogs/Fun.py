3f3501dd6ff53ef5c910120eb14e41aa98874803
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-04-30 18:02:59 +0530

    Fun commands added

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext.commands.core import command
 import requests
 from discord.ext import commands
 import random
@@ -12,6 +13,34 @@ class Fun(commands.Cog):
 
     def __init__(self, commands):
         self.commands = commands
+
+    @commands.command(aliases=['8ball', 'test', 'ask'])
+    async def _8ball(self, ctx, *, question):
+        ''' Ask question and get advice from me '''
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes - definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Outlook good.",
+                     "Yes.",
+                     "Signs point to yes.",
+                     "Reply hazy, try again.",
+                     "Ask again later."
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Outlook not so good.",
+                     "Very doubtful."]
+        em = discord.Embed(title='Magic 8ball!',
+                           colour=discord.Colour.orange())
+        em.add_field(name=f"**Question:** {question}",
+                     value=f"**Answer:** {random.choice(responses)}")
 
     @commands.command()
     @commands.cooldown(rate=1, per=2.0, type=commands.BucketType.user)
@@ -127,6 +156,99 @@ class Fun(commands.Cog):
             except:
                 await ctx.send("Sorry, I can find " + querry_ + " in Wikipedia")
 
+    @commands.command()
+    async def kill(self, ctx, user: discord.Member = None):
+        ''' kill someone'''
+        if user == None:
+            user = ctx.author
+        await ctx.send(f'{user.display_name} {random.choice(kills)}')
+
+    @commands.command(aliases=['ppsize', 'size', 'penis'])
+    async def pp(self, ctx, member: discord.Member):
+        ''' to check pp size '''
+        x = ['', '==', '', '=', '', '====', '', '=', '======', '==========================', '===', "===============",
+             "========", "===", "===================", "===", '========', '=====', "======================================", "===", "============"]
+        em = discord.Embed(color=discord.Colour.blue(),
+                           title="PeePee size calculator")
+        size = random.choice(x)
+        em.add_field(name=f"{member.display_name}s penis:eggplant:",
+                     value=f"8{random.choice(size)}D")
+        await ctx.send(embed=em)
+
+    @commands.command(aliases=['how gay', 'gaypercent'])
+    async def howgay(self, ctx, member: discord.Member):
+        ''' To check gayness '''
+        per = random.randint(0, 100)
+
+        if per >= 50:
+            gay = 'GAY'
+        else:
+            gay = "Not Gay"
+        em = discord.Embed(title=member.display_name,
+                           description=":two_men_holding_hands: gay result:", color=discord.Colour.red())
+        em.add_field(
+            name=gay, value=f"{member.display_name} is :rainbow_flag: {str(per)}% gay ")
+        em.set_thumbnail(url=member.avatar_url)
+
+        await ctx.send(embed=em)
+
+    @commands.command(aliases=['pass', 'generator', 'password', 'passwordgenerator'])
+    async def password(self, ctx, amt: int = 8):
+        ''' Generate random password  '''
+        try:
+            nwpss = []
+            lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@',
+                   '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', ",", '}', ']',
+                   '[', ';', ':', '<', '>', '?', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '`', '~', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            for x in range(amt):
+                newpass = random.choice(lst)
+                nwpss.append(newpass)
+            fnpss = ''.join(nwpss)
+            await ctx.send(f'{ctx.author} attempting to send you the genereated password in dms.')
+            await ctx.author.send(f':white_check_mark:Password Generated: {fnpss}')
+        except Exception as e:
+            print(e)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
+
+
+#----------------------------------------------------------------#
+kills = [
+    "died from a creeper explosion",
+    "died from lack of quality memes on Dank Memer",
+    "disappeared from the universe.",
+    "died from subscribing to t-series",
+    "died from not subscribing to Pewdiepie",
+    "ripped their own heart out to show their love for jhonny sins",
+    "dies because they were just too angry😡 ",
+    "chokes on cheerios and dies. What an idiot...",
+    "died from patta se head shot 🔫",
+    "was killed for being too noob.. what a noob",
+    "died after fapping 50 times in a row with no break.",
+    "because they were just too Lazy.",
+    "was killed by imposter",
+    "died from lack of ***, U got that 😁",
+    "drowned in their own tears 😭.",
+    "was eaten by the duolingo owl🦉 ",
+    "dies, but don't let this distract you from the fact that in 1998, The Undertaker threw",
+    "died from a nightmare👻 dream",
+    "died while playing hopscotch on seemingly deactivated land mines.",
+    "loses the will to live🤥",
+    "died from whacking it too much. (There's a healthy balance, boys)",
+    "eats too much copypasta and explodes",
+    "reads memes till they die.",
+    "dies of starvation.",
+    "died somehow 🤷‍♂️",
+    "died by taking sefi🤳 with lion🦁 ..such a idiot ",
+    "died in 2020 for not wearing mask😷",
+    "died from subscribing to t-series",
+    "is dead. How 🤷🏼‍♂️",
+    "died of scurvy, eat oranges kids",
+    "cranks up the music system only to realize the volume was at max and the song playing was Baby by Justin Beiber..",
+    "cranks up the music system only to realize the volume was at max and the song playing was selfie Maine le liya..",
+    'died from watching to much of P**n 🤦‍♂️',
+    'died from COVID-69, new virus transmitted from ga* s** 🚫🧑‍🤝‍🧑',
+]
