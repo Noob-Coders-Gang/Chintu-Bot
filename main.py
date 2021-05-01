679cf82800eb9a855afb7c5e802be7aa4f6b6809
Devansh-bit <67866743+Devansh-bit@users.noreply.github.com>
2021-05-01 19:14:42 +0530

    enabled on_command_error

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ change_status = tasks.loop(seconds=300)(loops.change_status)
 
 #--------------------------------Events--------------------------------#
 events = Events(bot, database, total_guilds_api_url)
-#bot.event(events.on_command_error)
+bot.event(events.on_command_error)
 bot.event(events.on_message)
 bot.event(events.on_guild_join)
 
