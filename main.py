59f52e9f1da412935efe3137abf059bd4bfd62ee
Swasthik Shetty <62538932+swasthikshetty10@users.noreply.github.com>
2021-05-08 10:12:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ loops = Loops(bot, custom_statuses)
 change_status = tasks.loop(seconds=60)(loops.change_status)
 
 # --------------------------------Events--------------------------------#
-events = Events(bot, database, total_guilds_api_url, ChintuAI=True)
+events = Events(bot, database, total_guilds_api_url, ChintuAI=False)
 bot.event(events.on_command_error)
 bot.event(events.on_message)
 bot.event(events.on_guild_join)
