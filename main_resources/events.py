30eee58f65f5ff7d1467216b2dad7bf17f07c8ea
Devansh-bit <67866743+Devansh-bit@users.noreply.github.com>
2021-05-17 00:55:46 +0530

    nick change bug fixed

--- main_resources/events.py
+++ main_resources/events.py
@@ -80,7 +80,7 @@ class Events:
 
     async def on_update_prefix(self, ctx, prefix):
         update_guild_storage(self.guild_prefix_store, ctx.guild.id, prefix)
-        await ctx.guild.me.edit(nick=f"{self.bot.user.name} [{prefix}]")
+        await ctx.guild.me.edit(nick=self.bot.user.name.replace(self.default_prefix, prefix))
 
     async def on_add_command(self, ctx, command_name):
         try:
