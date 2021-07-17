32d5ede0d9483b6bbe70e8ab07cf838c33f36a9d
Devansh-bit <67866743+Devansh-bit@users.noreply.github.com>
2021-07-17 12:09:29 +0530

    fixed mute

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -4,8 +4,20 @@ from datetime import datetime
 
 import discord
 from discord.ext import commands
-
+from discord.utils import find
 import main
+
+
+class BannedUser(commands.Converter):
+    async def convert(self, ctx, arg):
+        banned = [e.user for e in await ctx.guild.bans()]
+        if banned:
+            user = find(lambda u: str(u) == arg, banned)
+            if user:
+                return user
+            else:
+                raise commands.BadArgument
+
 
 
 class Moderation(commands.Cog):
@@ -89,21 +101,19 @@ class Moderation(commands.Cog):
                 timetuple = warn['time']
                 embed.add_field(
                     name="Warned at",
-                    value=f"{timetuple[2]}/{timetuple[1]}/{timetuple[0]} {timetuple[3]}:{timetuple[4]} (UTC)", inline=False)
+                    value=f"{timetuple[2]}/{timetuple[1]}/{timetuple[0]} {timetuple[3]}:{timetuple[4]} (UTC)",
+                    inline=False)
                 await ctx.send(embed=embed)
             else:
                 await ctx.send(f"{ctx.author.mention} Warn not found!")
         else:
             await ctx.send(f"{ctx.author.mention} Warn not found!")
 
-
-
     @commands.command(name="kick")
     @commands.has_permissions(kick_members=True)
     @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
     async def kick(self, ctx, member: discord.Member, *, reason):
         """Kicks the mentioned member"""
-        print(reason)
         embed = discord.Embed(
             description=str(
                 str(member) + " is Kicked | reason = " + reason),
@@ -115,43 +125,53 @@ class Moderation(commands.Cog):
     @commands.command(name="ban")
     @commands.has_permissions(ban_members=True)
     @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
-    async def ban(self, ctx, member: discord.Member, *, reason):
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
         """Bans the mentioned member"""
-        print(reason)
+        if not reason:
+            reason = "No reason was provided"
         embed = discord.Embed(
-            description=str(
-                str(member) + " is banned | reason = " + reason),
+            description=str(member) + " has been banned | reason = " + reason,
             colour=discord.Colour.green()
         )
-        await member.send(embed=embed)
-        await member.ban(reason=reason)
+        try:
+            await member.send(embed=embed)
+        except Exception:
+            pass
+        finally:
+            await member.ban(reason=reason)
 
     @commands.command(name="mute")
-    @commands.has_permissions(kick_members=True)
+    @commands.has_permissions(manage_roles=True)
     @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
-    async def mute(self, ctx, member: discord.Member, *, reason):
-        """Gives muted role to the mentioned user"""
-        print(reason)
-        Muted = discord.utils.get(ctx.guild.roles, name="Muted")
-        await member.add_roles(Muted)
-        embed = discord.Embed(
-            description=str(
-                str(member) + " is Muted | reason = " + reason),
-            colour=discord.Colour.red()
-        )
-        await member.send(embed=embed)
+    async def mute(self, ctx: commands.Context, member: discord.Member, *, reason=None):
+        mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+        if not mutedRole:
+            bot_integration_role = ctx.guild.me.roles[-1]
+            mutedRole = await ctx.guild.create_role(name="Muted", color=0x000000)
+            await mutedRole.edit(position=bot_integration_role.position-1)
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(mutedRole, send_messages=False)
+        if not reason:
+            reason = "No reason was provided."
+        embed = discord.Embed(title=str(str(member) + " has been Muted | reason = " + reason),
+                              colour=discord.Colour.red())
+        await ctx.send(embed=embed)
+        await member.add_roles(mutedRole, reason=reason)
+        try:
+            await member.send(f" you have been muted in {ctx.guild.name} | Reason: {reason}")
+        except Exception:
+            return
 
     @commands.command(name="unmute")
     @commands.has_permissions(kick_members=True)
     @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
     async def unmute(self, ctx, member: discord.Member, *, reason="No reason specified"):
         """Unmutes the mentioned member"""
-        print(reason)
         Muted = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.remove_roles(Muted)
         embed = discord.Embed(
             description=str(
-                str(member) + " is Unmuted | reason = " + reason),
+                str(member) + " has been Unmuted | reason = " + reason),
             colour=discord.Colour.green()
         )
         await member.send(embed=embed)
